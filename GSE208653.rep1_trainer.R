0fb3c773f50b392066f830ec336b7f69ef56f116
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-09-26 12:23:38 -1000

    Create GSE208653.rep1_trainer.R

---
+++ GSE208653.rep1_trainer.R
@@ -0,0 +1,123 @@
+library(Seurat)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(ggridges)
+library(caTools)
+library(car)
+library(caret)
+library(InformationValue)
+library(pROC)
+library(ROCR)
+setwd()
+#---------------------------------------------------------------------------
+features_path <- 'GSM6360682_N_1.features.tsv.gz'
+barcodes_path <- 'GSM6360682_N_1.barcodes.tsv.gz'
+matrix_path <- 'GSM6360682_N_1.matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+hpv <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'hpv')
+gc()
+#---------------------------------------------------------------------------
+features_path <- 'GSM6360684_HSIL_1.features.tsv.gz'
+barcodes_path <- 'GSM6360684_HSIL_1.barcodes.tsv.gz'
+matrix_path <- 'GSM6360684_HSIL_1.matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+lesions <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'lesions')
+gc()
+#---------------------------------------------------------------------------
+features_path <- 'GSM6360686_SCC_4.features.tsv.gz'
+barcodes_path <- 'GSM6360686_SCC_4.barcodes.tsv.gz'
+matrix_path <- 'GSM6360686_SCC_4.matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+cervical_cancer <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'cervical_cancer')
+gc()
+#-------------------------------------------------------------------------------
+data<-merge(hpv,y=c(lesions,cervical_cancer),project='hpv_cervical_cancer')
+table(data@meta.data$orig.ident)
+rm(hpv,lesions,cervical_cancer,matrix)
+gc()
+data[["percent.mt"]] <- PercentageFeatureSet(data, pattern = "^MT-")
+VlnPlot(data, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol=3)
+data <- subset(data, subset = nFeature_RNA > 200 & nFeature_RNA < 9000 & percent.mt <20)
+VlnPlot(data, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol=3)
+FeatureScatter(data, feature1 = "nCount_RNA", feature2 = "nFeature_RNA") + geom_smooth()
+data <- NormalizeData(data)
+gc()
+data <- FindVariableFeatures(data, selection.method = "vst", nfeatures = 2000)
+VariableFeatures(data)
+top1000 <- head(VariableFeatures(data), 1000)
+gc()
+plot1 <- VariableFeaturePlot(data)
+plot1
+all.genes <- rownames(data)
+all.genes
+gc()
+data <- ScaleData(data, features = all.genes)
+gc()
+dim(data)
+data <- RunPCA(data, features = VariableFeatures(object = data))
+DimHeatmap(data, dims = 1:15, cells = 500, balanced = T)
+ElbowPlot(data)
+gc()
+table(data@meta.data$orig.ident)
+VlnPlot(data, features = c('PTPRC','TRAC','CD19','CD14','MILR1'),cols = c())
+#----------Isolate CD45+  -------------------------------------
+data$CD45.groups <- 'CD45.pos'
+data$CD45.groups[WhichCells(data, expression= PTPRC < 0.1)] <- 'CD45.neg'
+DimPlot(data, reduction = 'pca',split.by = 'CD45.groups')
+head(data@meta.data)
+data <- subset(data, subset = CD45.groups != "CD45.neg")
+gc()
+table(data@meta.data$orig.ident)
+#-------------CD45+ CD19-  ---------------------------------------------------------
+data$CD19.groups <- 'CD19.pos'
+data$CD19.groups[WhichCells(data, expression= CD19 < 0.1)] <- 'CD19.neg'
+DimPlot(data, reduction = 'pca',split.by = 'CD19.groups')
+head(data@meta.data)
+data <- subset(data, subset = CD19.groups != "CD19.pos")
+gc()
+table(data@meta.data$orig.ident)
+#-------------CD45+ CD19- TRAC- -------------------------------------------
+data$TRAC.groups <- 'TRAC.pos'
+data$TRAC.groups[WhichCells(data, expression= TRAC < 0.1)] <- 'TRAC.neg'
+DimPlot(data, reduction = 'pca',split.by = 'TRAC.groups')
+head(data@meta.data)
+data <- subset(data, subset = TRAC.groups != "TRAC.pos")
+gc()
+table(data@meta.data$orig.ident)
+#-------------CD45+ CD19- TRAC- CD14+     -------------------------------------------
+data$CD14.groups <- 'CD14.pos'
+data$CD14.groups[WhichCells(data, expression= CD14 < 0.1)] <- 'CD14.neg'
+DimPlot(data, reduction = 'pca',split.by = 'CD14.groups')
+head(data@meta.data)
+data <- subset(data, subset = CD14.groups != "CD14.neg")
+gc()
+table(data@meta.data$orig.ident)
+#----------Isolate MILR1+  -------------------------------------
+data$MILR1.groups <- 'MILR1.pos'
+data$MILR1.groups[WhichCells(data, expression= MILR1 < 0.1)] <- 'MILR1.neg'
+DimPlot(data, reduction = 'pca',split.by = 'MILR1.groups')
+head(data@meta.data)
+data <- subset(data, subset = MILR1.groups != "MILR1.neg")
+gc()
+table(data@meta.data$orig.ident)
+VlnPlot(data, features = c('PTPRC','TRAC','CD19','CD14','MILR1'),cols = c())
+#----------------------------------------------------------------
+reg<-FetchData(data,vars = c('ident','CXCL9','IFI6','SLC40A1',slot = 'counts'))
+table(reg$ident)
+reg$ident<-ifelse(reg$ident=='cervical_cancer', 1, 0)
+table(reg$ident)
+edit<-reg[-c(417:496),]
+table(edit$ident)
+reg<-edit[sample(1:nrow(edit)),]
+table(reg$ident)
+#----------run model
+model<-glm(ident~CXCL9+IFI6+SLC40A1,data = reg, family = binomial)
+summary(model)
+vif(model)
+varImp(model)
+logLik(model)
+#----------------------------
+setwd()
+write_rds(model,file = 'cc_model.rda')
+break 

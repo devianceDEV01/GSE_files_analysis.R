064de91841fee73e6d217ba3469387764b06deac
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2023-10-01 10:26:56 -1000

    Update GSE208653.rep1.rep2.trainer+validations.R

--- GSE208653.rep1.rep2.trainer+validations.R
+++ GSE208653.rep1.rep2.trainer+validations.R
@@ -58,59 +58,39 @@ plot1 <- VariableFeaturePlot(data)
 plot1
 all.genes <- rownames(data)
 all.genes
-gc()
-data <- ScaleData(data, features = all.genes)
-gc()
-dim(data)
-data <- RunPCA(data, features = VariableFeatures(object = data))
-DimHeatmap(data, dims = 1:15, cells = 500, balanced = T)
-ElbowPlot(data)
-gc()
 table(data@meta.data$orig.ident)
 VlnPlot(data, features = c('PTPRC','TRAC','CD19','CD14','MILR1'),cols = c())
 #----------Isolate CD45+  -------------------------------------
 data$CD45.groups <- 'CD45.pos'
 data$CD45.groups[WhichCells(data, expression= PTPRC < 0.1)] <- 'CD45.neg'
-DimPlot(data, reduction = 'pca',split.by = 'CD45.groups')
-head(data@meta.data)
 data <- subset(data, subset = CD45.groups != "CD45.neg")
 gc()
 table(data@meta.data$orig.ident)
 #-------------CD45+ CD19-  ---------------------------------------------------------
 data$CD19.groups <- 'CD19.pos'
 data$CD19.groups[WhichCells(data, expression= CD19 < 0.1)] <- 'CD19.neg'
-DimPlot(data, reduction = 'pca',split.by = 'CD19.groups')
-head(data@meta.data)
 data <- subset(data, subset = CD19.groups != "CD19.pos")
 gc()
 table(data@meta.data$orig.ident)
 #-------------CD45+ CD19- TRAC- -------------------------------------------
 data$TRAC.groups <- 'TRAC.pos'
 data$TRAC.groups[WhichCells(data, expression= TRAC < 0.1)] <- 'TRAC.neg'
-DimPlot(data, reduction = 'pca',split.by = 'TRAC.groups')
-head(data@meta.data)
 data <- subset(data, subset = TRAC.groups != "TRAC.pos")
 gc()
 table(data@meta.data$orig.ident)
 #-------------CD45+ CD19- TRAC- CD14+     -------------------------------------------
 data$CD14.groups <- 'CD14.pos'
 data$CD14.groups[WhichCells(data, expression= CD14 < 0.1)] <- 'CD14.neg'
-DimPlot(data, reduction = 'pca',split.by = 'CD14.groups')
-head(data@meta.data)
 data <- subset(data, subset = CD14.groups != "CD14.neg")
 gc()
 table(data@meta.data$orig.ident)
 #----------Isolate MILR1+  -------------------------------------
 data$MILR1.groups <- 'MILR1.pos'
 data$MILR1.groups[WhichCells(data, expression= MILR1 < 0.1)] <- 'MILR1.neg'
-DimPlot(data, reduction = 'pca',split.by = 'MILR1.groups')
-head(data@meta.data)
 data <- subset(data, subset = MILR1.groups != "MILR1.neg")
 gc()
 table(data@meta.data$orig.ident)
-VlnPlot(data, features = c('PTPRC','TRAC','CD19','CD14','MILR1'),cols = c())
 #------------------------------------------------
-gc()
 set.seed(14)
 reg<-FetchData(data,vars = c('ident','CXCL10','FN1','SDC3'),slot = 'counts')
 table(reg$ident)
@@ -129,12 +109,11 @@ test   <- reg[!sample, ]
 table(test$ident)
 #
 model<-glm(ident~CXCL10+FN1+SDC3,data = train, family = binomial)
-vif(model)
 summary(model)
 logLik(model)
 #------Displaying variance inflation factors
 vif(model)
-#------Displaying variable importance factors
+#------Displaying variable importances
 varImp(model)
 newdata = test
 summary(newdata)

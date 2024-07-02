04f18e4742682b735296971573291325e156b5a4
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-07-01 20:12:06 -0400

    Update GSE237779_Fig.1A.B.R

--- GSE237779_Fig.1A.B.R
+++ GSE237779_Fig.1A.B.R
@@ -1,28 +1,14 @@
 library(Seurat)
-library(tidyverse)
-library(dplyr)
-library(ggplot2)
-library(ggridges)
-library(caTools)
-library(car)
-library(caret)
-library(InformationValue)
-library(pROC)
-library(ROCR)
-setwd()
-data<-read_rds('GSE237779_integrated_seurat_object_CD45_scRNAseq.rds')
-#----------Isolate CD45+  -------------------------------------
-data$CD45.groups <- 'CD45.pos'
-data$CD45.groups[WhichCells(data, expression= PTPRC < 0.1)] <- 'CD45.neg'
-head(data@meta.data)
-data <- subset(data, subset = CD45.groups != "CD45.neg")
-gc()
+library(readr)
+setwd('/home/em_b/Desktop/scRNAseq_manuscript/Fig.A.B')
+data<-read_rds('integrated_seurat_object_CD45_scRNAseq.rds')
 VlnPlot(data,features = 'PTPRC')
-Idents(data)<-data@meta.data$integrated_celltype
 #----  Figure 1a
-VlnPlot(data, features = c('MILR1'),pt.size=0.1,cols = c('red','grey','grey','grey','grey','grey','grey','grey','red','grey','grey','grey','grey','grey','grey','grey','grey','grey','red'))
-FindMarkers(data, ident.1 = 'Macrophage', ident.2 = 'CD4+ Tcm', features = c('MILR1'))
+VlnPlot(data, features = c('MILR1'),pt.size=0.1,cols = c('red','grey','grey','grey','grey','grey','grey','grey'))
+FindMarkers(data, ident.1 = 'Mono/Macro', ident.2 = 'CD4+ T', features = c('MILR1'),
+            test.use='negbinom')
 #----- Figure 1b
-VlnPlot(data, features = c('MRC1'),pt.size=0.1,cols = c('red','grey','grey','grey','grey','grey','grey','grey','red','grey','grey','grey','grey','grey','grey','grey','grey','grey','red'))
-FindMarkers(data, ident.1 = 'Macrophage', ident.2 = 'CD4+ Tcm', features = c('MRC1'))
+VlnPlot(data, features = c('MRC1'),pt.size=0.1,cols = c('red','grey','grey','grey','grey','grey','grey','grey'))
+FindMarkers(data, ident.1 = 'Mono/Macro', ident.2 = 'CD4+ T', features = c('MRC1'),
+            test.use='negbinom')
 

fbaefeb2bcc5acf188cd8846ccb1afb6ae16a5a4
devianceDEV01 <121775590+devianceDEV01@users.noreply.github.com>
2024-07-03 22:37:55 -0400

    Update and rename GSE145370.pt1.R to GSE145370_pt1_Fig.6C.D.R

--- GSE145370_pt1_Fig.6C.D.R
+++ GSE145370_pt1_Fig.6C.D.R
@@ -9,57 +9,74 @@ library(caret)
 library(InformationValue)
 library(pROC)
 library(ROCR)
-setwd('P1.A')
-barcodes_path <- 'barcodes.tsv.gz'
-features_path <- 'features.tsv.gz'
-matrix_path <- 'matrix.mtx.gz'
-matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
-a1 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
-table(a1@meta.data$orig.ident)
 #---------------------------------------------------------------------------
-setwd('P2.A')
+setwd('/home/em_b/Desktop/scRNAseq_manuscript/Fig.6/GSE145370_RAW/S133B_adjacent/filtered_feature_bc_matrix')
 barcodes_path <- 'barcodes.tsv.gz'
 features_path <- 'features.tsv.gz'
 matrix_path <- 'matrix.mtx.gz'
 matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
-a2 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
-table(a2@meta.data$orig.ident)
+adjacent1 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
+table(adjacent1@meta.data$orig.ident)
 #-------------------------------------------------------------------------------
-setwd('P3.A')
+setwd('/home/em_b/Desktop/scRNAseq_manuscript/Fig.6/GSE145370_RAW/S134B_adjacent/filtered_feature_bc_matrix')
 barcodes_path <- 'barcodes.tsv.gz'
 features_path <- 'features.tsv.gz'
 matrix_path <- 'matrix.mtx.gz'
 matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
-a3 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
-table(a3@meta.data$orig.ident)
+adjacent2 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
+table(adjacent2@meta.data$orig.ident)
 #-------------------------------------------------------------------------------
-setwd('P4.A')
+setwd('/home/em_b/Desktop/scRNAseq_manuscript/Fig.6/GSE145370_RAW/S135B_adjacent/filtered_feature_bc_matrix')
 barcodes_path <- 'barcodes.tsv.gz'
 features_path <- 'features.tsv.gz'
 matrix_path <- 'matrix.mtx.gz'
 matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
-a4 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
-table(a4@meta.data$orig.ident)
+adjacent3 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
+table(adjacent3@meta.data$orig.ident)
 #-------------------------------------------------------------------------------
-setwd('P5.A')
+setwd('/home/em_b/Desktop/scRNAseq_manuscript/Fig.6/GSE145370_RAW/S149B_adjacent/filtered_feature_bc_matrix')
 barcodes_path <- 'barcodes.tsv.gz'
 features_path <- 'features.tsv.gz'
 matrix_path <- 'matrix.mtx.gz'
 matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
-a5 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
-table(a5@meta.data$orig.ident)
+adjacent4 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
+table(adjacent4@meta.data$orig.ident)
 #---------------------------------------------------------------------------
-setwd('P1.T')
+setwd('/home/em_b/Desktop/scRNAseq_manuscript/Fig.6/GSE145370_RAW/S150B_adjacent/filtered_feature_bc_matrix')
 barcodes_path <- 'barcodes.tsv.gz'
 features_path <- 'features.tsv.gz'
 matrix_path <- 'matrix.mtx.gz'
 matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
-e1 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'ESCC')
-table(e1@meta.data$orig.ident)
+adjacent5 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
+table(adjacent5@meta.data$orig.ident)
+#---------------------------------------------------------------------------
+setwd('/home/em_b/Desktop/scRNAseq_manuscript/Fig.6/GSE145370_RAW/S158B_adjacent/filtered_feature_bc_matrix')
+barcodes_path <- 'barcodes.tsv.gz'
+features_path <- 'features.tsv.gz'
+matrix_path <- 'matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+adjacent6 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
+table(adjacent6@meta.data$orig.ident)
+#---------------------------------------------------------------------------
+setwd('/home/em_b/Desktop/scRNAseq_manuscript/Fig.6/GSE145370_RAW/S159B_adjacent/filtered_feature_bc_matrix')
+barcodes_path <- 'barcodes.tsv.gz'
+features_path <- 'features.tsv.gz'
+matrix_path <- 'matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+adjacent7 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'adjacent')
+table(adjacent7@meta.data$orig.ident)
 #-------------------------------------------------------------------------------
-data<-merge(a1,y=c(a2,a3,a4,a5,e1),project='ESCC')
-rm(a1,a2,a3,a4,a5,e1,matrix,features_path,barcodes_path,matrix_path)
-head(data@active.ident)
+setwd('/home/em_b/Desktop/scRNAseq_manuscript/Fig.6/GSE145370_RAW/S133A_ESCC/filtered_feature_bc_matrix')
+barcodes_path <- 'barcodes.tsv.gz'
+features_path <- 'features.tsv.gz'
+matrix_path <- 'matrix.mtx.gz'
+matrix <- ReadMtx(mtx= matrix_path, features = features_path, cells= barcodes_path)
+escc1 <- CreateSeuratObject(counts=matrix,min.cells=20,min.features=200,project = 'ESCC')
+table(escc1@meta.data$orig.ident)
+#-------------------------------------------------------------------------------
+data<-merge(adjacent1,y=c(adjacent2,adjacent3,adjacent4,adjacent5,
+                          adjacent6,adjacent7,escc1),project='ESCC')
+summary(data@active.ident)
 gc()
 data[["percent.mt"]] <- PercentageFeatureSet(data, pattern = "^MT-")
 VlnPlot(data, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol=3)
@@ -84,11 +101,9 @@ data <- RunPCA(data, features = VariableFeatures(object = data))
 DimHeatmap(data, dims = 1:15, cells = 500, balanced = T)
 ElbowPlot(data)
 gc()
-VlnPlot(data, features = c('PTPRC','CD19','TRAC','CD14','MILR1'),pt.size=0.1)
 #----------Isolate CD45+  -------------------------------------
 data$CD45.groups <- 'CD45.pos'
 data$CD45.groups[WhichCells(data, expression= PTPRC < 0.1)] <- 'CD45.neg'
-DimPlot(data, reduction = 'pca',split.by = 'CD45.groups')
 head(data@meta.data)
 data <- subset(data, subset = CD45.groups != "CD45.neg")
 gc()
@@ -96,7 +111,6 @@ table(data@meta.data$orig.ident)
 #-------------CD45+ CD19-  ---------------------------------------------------------
 data$CD19.groups <- 'CD19.pos'
 data$CD19.groups[WhichCells(data, expression= CD19 < 0.1)] <- 'CD19.neg'
-DimPlot(data, reduction = 'pca',split.by = 'CD19.groups')
 head(data@meta.data)
 data <- subset(data, subset = CD19.groups != "CD19.pos")
 gc()
@@ -104,7 +118,6 @@ table(data@meta.data$orig.ident)
 #-------------CD45+ CD19- TRAC- -------------------------------------------
 data$TRAC.groups <- 'TRAC.pos'
 data$TRAC.groups[WhichCells(data, expression= TRAC < 0.1)] <- 'TRAC.neg'
-DimPlot(data, reduction = 'pca',split.by = 'TRAC.groups')
 head(data@meta.data)
 data <- subset(data, subset = TRAC.groups != "TRAC.pos")
 gc()
@@ -112,7 +125,6 @@ table(data@meta.data$orig.ident)
 #-------------CD45+ CD19- TRAC- CD14+     -------------------------------------------
 data$CD14.groups <- 'CD14.pos'
 data$CD14.groups[WhichCells(data, expression= CD14 < 0.1)] <- 'CD14.neg'
-DimPlot(data, reduction = 'pca',split.by = 'CD14.groups')
 head(data@meta.data)
 data <- subset(data, subset = CD14.groups != "CD14.neg")
 gc()
@@ -120,21 +132,26 @@ table(data@meta.data$orig.ident)
 #----------Isolate MILR1+  -------------------------------------
 data$MILR1.groups <- 'MILR1.pos'
 data$MILR1.groups[WhichCells(data, expression= MILR1 < 0.1)] <- 'MILR1.neg'
-DimPlot(data, reduction = 'pca',split.by = 'MILR1.groups')
 head(data@meta.data)
 data <- subset(data, subset = MILR1.groups != "MILR1.neg")
 gc()
 table(data@meta.data$orig.ident)
-VlnPlot(data, features = c('MILR1'),cols = c())
+VlnPlot(data, features = c('PTPRC','CD19','TRAC','CD14','MILR1'),pt.size=0.1)
+data<-JoinLayers(data)
 #--------------------------------------------------------------
 set.seed(10)
 #----split data
-reg<-FetchData(data,vars = c('ident','C1QC','PMP22','CLEC5A'),slot = 'counts')
+reg<-FetchData(data,vars = c('ident','HSPA1B','MMP12',
+                             'MT-ATP8','CTSC',
+                             'IER5','FTL',
+                             'ACP5','MMP9',
+                             'ID3','GSN','CLEC11A','RNASE1',
+                             'CTSD','C15orf48'),slot = 'counts')
 table(reg$ident)
 reg$ident<-ifelse(reg$ident=='ESCC', 1, 0)
 table(reg$ident)
 #------Even out group numbers and shuffle
-edit<-reg[-c(433:730),]
+edit<-reg[-c(575:801),]
 table(edit$ident)
 reg<-edit[sample(1:nrow(edit)),]
 table(reg$ident)
@@ -144,7 +161,13 @@ table(train$ident)
 test   <- reg[!sample, ]
 table(test$ident)
 #-------------------------------------------------------------------------
-model<-glm(ident~C1QC+PMP22+CLEC5A,data = train, family = binomial)
+model<-glm(ident~HSPA1B+MMP12+
+             `MT-ATP8`+CTSC+
+             IER5+FTL+
+             ACP5+MMP9+
+             ID3+GSN+CLEC11A+RNASE1+
+             CTSD+C15orf48,
+           data = train, family = binomial)
 summary(model)
 logLik(model)
 #------Displaying variance inflation factors
@@ -182,10 +205,32 @@ logLik(model)
 confusion_matrix
 table(data@meta.data$orig.ident)
 table(test$ident)
-break 
-#----Figure 6a
-VlnPlot(data, features = c('C1QC','PMP22','CLEC5A'),cols = c('grey','yellow'))
-FindMarkers(data, ident.1 = 'ESCC', ident.2 = 'adjacent', features = c('C1QC','PMP22','CLEC5A'))
-#----Figure 6b
-fourfoldplot(as.table(confusion_matrix),color = c('grey','yellow'),main='adjacent=0 ESCC=1')
-plot.roc(actuals, predicted, percent = TRUE, main = 'ESCC_validation_ROC', add =  FALSE, asp = NA, print.auc = TRUE)
+#----Figure 6C
+VlnPlot(data, features = c('HSPA1B','MMP12',
+                           'MT-ATP8','CTSC',
+                           'IER5','FTL',
+                           'ACP5','MMP9',
+                           'ID3','GSN','CLEC11A','RNASE1',
+                           'CTSD','C15orf48'),cols = c('grey','yellow'))
+#----Figure 6D
+fourfoldplot(as.table(confusion_matrix),
+             color = c('skyblue','yellow'),
+             std='all.max',
+             main='Adjacent=0 ESCC=1')
+plot.roc(actuals, predicted,
+         percent = TRUE,
+         main = 'ESCC patient 1 Area Under the ROC curve',
+         add =  FALSE,
+         asp = NA,
+         print.auc = TRUE,
+         print.auc.col='red',
+         print.auc.cex=1,
+         grid=TRUE,
+         grid.col='skyblue',
+         identity.col='blue',
+         identity.lty=8,
+         col='red',
+         print.thres=TRUE,
+         print.thres.pch=5,
+         print.thres.col='black',
+         ci=TRUE)
